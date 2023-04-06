ea0f67beb07c69f95854dadc75667a4ea3b4bd74
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-04-06 20:05:36 +0500

    Add project files.

---
+++ HybridFramework.Test/Pages/LoginPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridFramework.Test.Pages;
+
+public class LoginPage
+{
+    private readonly IWebDriver _driver;
+
+    private IWebElement UsernameField => _driver.FindElement(By.Id("username"));
+    private IWebElement PasswordField => _driver.FindElement(By.Id("password"));
+    private IWebElement SubmitButton => _driver.FindElement(By.Id("submit"));
+
+    public LoginPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    public void Login(string username, string password)
+    {
+        UsernameField.SendKeys(username);
+        PasswordField.SendKeys(password);
+        SubmitButton.Click();
+    }
+}
---
+++ HybridFramework.Test/WebDriverManager.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+#nullable disable
+
+namespace HybridFramework.Test;
+
+public class WebDriverManager
+{
+    private static IWebDriver _driver;
+
+    public static IWebDriver Driver
+    {
+        get
+        {
+            if (_driver == null)
+            {
+                _driver = new ChromeDriver();
+            }
+            return _driver;
+        }
+    }
+
+    public static void QuitDriver()
+    {
+        _driver.Quit();
+        _driver = null;
+    }
+
+    public static void TakeScreenshot(string testName)
+    {
+        string screenshotPath = Path.Combine("Screenshots", testName + "_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png");
+        Screenshot screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+        screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+        {
+            TakeScreenshot(TestContext.CurrentContext.Test.Name);
+        }
+        QuitDriver();
+    }
+
+}
