c7253b4fa997967792489eb6dc71340324728140
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-04-24 14:52:46 +0500

    2.0.1

--- HybridFramework.Test/Tests/GoogleCloudPricingCalcTests.cs
+++ HybridFramework.Test/Tests/GoogleCloudPricingCalcTests.cs
@@ -8,14 +8,12 @@ namespace HybridFramework.Test.Tests;
 public class GoogleCloudPricingCalcTests
 {
     private IWebDriver _driver;
-    private WebDriverWait _wait;
 
     [SetUp]
     public void SetUp()
     {
         _driver = new ChromeDriver();
         _driver.Manage().Window.Maximize();
-        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
     }
 
     [Test]
@@ -25,7 +23,6 @@ public class GoogleCloudPricingCalcTests
         loginPage.GoToPage();
         loginPage.EnterEmail_and_ClickNext("fakeepamacc@gmail.com");
         loginPage.EnterPassword_and_ClickNext("s944257242S@");
-
 
         GoogleCloudPricingCalcPage googleCloudPricingCalcPage = new GoogleCloudPricingCalcPage(_driver);
         googleCloudPricingCalcPage.GoToPage();
@@ -41,26 +38,21 @@ public class GoogleCloudPricingCalcTests
         googleCloudPricingCalcPage.Click_and_SelectCommitedUsage();
         googleCloudPricingCalcPage.ClickAddToEstimate();
         string estimatedCost = googleCloudPricingCalcPage.CopyEstimatedCost();
-        Thread.Sleep(1000);
 
         EmailGeneratorPage emailGeneratorPage = new EmailGeneratorPage(_driver);
         emailGeneratorPage.GoToPage();
         emailGeneratorPage.GenerateEmail();
         string email = emailGeneratorPage.CopyEmail();
-        Thread.Sleep(500);
 
         googleCloudPricingCalcPage.PopUpEmailWindow();
         googleCloudPricingCalcPage.Click_and_TypeEmail(email);
         googleCloudPricingCalcPage.ClickSendEmailBtn();
 
-        Thread.Sleep(1000);
         emailGeneratorPage.ClickEmailInbox();
         emailGeneratorPage.ClickRefresh();
         string totalCost = emailGeneratorPage.GetTotalCost();
-        
 
         Assert.That(totalCost, Is.EqualTo(estimatedCost));
-        Thread.Sleep(1000);
     }
 
     [TearDown]
